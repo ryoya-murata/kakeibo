27677073bef15d98444fdec6080dc34a6225c55b
ryoya-murata <ryoya.working@gmail.com>
2022-08-28 12:45:13 +0000

    Create Update screen

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,4 +25,8 @@ class BooksController < ApplicationController
         
     end
     
+    def edit
+        @book = Book.find(params[:id])
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     get "/books", to: "books#index"
     post "/books", to: "books#create"
     get "/books/new", to: "books#new", as: "new_book"
+    get "/books/:id/edit", to: "books#edit", as: "edit_book"
     get "/books/:id", to: "books#show", as: "book"
 end
