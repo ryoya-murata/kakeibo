9b68b1086a706ec0a8989f5f96995b20099a456a
ryoya-murata <ryoya.working@gmail.com>
2022-08-29 02:59:20 +0000

    Add flash function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,9 +17,11 @@ class BooksController < ApplicationController
         # 新しいインスタンスの生成
         @book = Book.new(book_params)
         if @book.save
+            flash[:notice] = "家計簿に新規データを登録しました"
             # 一覧画面へのリダイレクト
             redirect_to books_path
         else
+            flash.now[:alert] = "登録に失敗しました"
             render :new
         end
         
@@ -34,9 +36,11 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])
         book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
         if @book.update(book_params)
+            flash[:notice] = "データを更新しました"
             # 一覧画面へのリダイレクト
             redirect_to books_path
         else
+            flash.now[:alert] = "データの更新に失敗しました"
             render :edit
         end
     end
@@ -44,6 +48,7 @@ class BooksController < ApplicationController
     def destroy
         @book = Book.find(params[:id])
         @book.destroy
+        flash[:notice] = "データを削除しました"
         redirect_to books_path
     end
     
