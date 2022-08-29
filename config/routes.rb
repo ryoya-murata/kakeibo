a5f50968ab268a4f03ba8d330996e6f3f6998488
ryoya-murata <ryoya.working@gmail.com>
2022-08-29 06:56:54 +0000

    Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-    # get "/books", to: "books#index"
-    # # 新規作成
-    # post "/books", to: "books#create"
-    # get "/books/new", to: "books#new", as: "new_book"
-    # get "/books/:id/edit", to: "books#edit", as: "edit_book"
-    # get "/books/:id", to: "books#show", as: "book"
-    # # 更新
-    # patch "/books/:id", to: "books#update"
-    # # 削除
-    # delete "/books/:id", to: "books#destroy"
+     root 'books#index'
+    
+    get "/books", to: "books#index"
+    # 新規作成
+    post "/books", to: "books#create"
+    get "/books/new", to: "books#new", as: "new_book"
+    get "/books/:id/edit", to: "books#edit", as: "edit_book"
+    get "/books/:id", to: "books#show", as: "book"
+    # 更新
+    patch "/books/:id", to: "books#update"
+    # 削除
+    delete "/books/:id", to: "books#destroy"
     
     # routeの自動生成
-    resources :books
+    # resources :books
 end
