485ffa6832820dc648f85957ddc086841ed65ffd
ryoya-murata <ryoya.working@gmail.com>
2022-09-02 00:34:27 +0000

    Create User model

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.10.3', require: false
+gem 'bcrypt',     '3.1.7'
 
 group :development, :test do
   gem 'sqlite3', '1.4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_055242) do
+ActiveRecord::Schema.define(version: 2022_09_02_003149) do
 
   create_table "books", force: :cascade do |t|
     t.integer "inout"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_08_22_055242) do
     t.integer "year"
     t.integer "month"
     t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
