d398eeaa8a6cc293602432b9b84402065fe38a2b
ryoya-murata <ryoya.working@gmail.com>
2022-09-03 06:03:31 +0000

    Create member create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
     
-    def 
+    def new
         @user = User.new
+    end
+    
+    def create
+        user_params = params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "登録しました"
+            # ログイン画面に
+        else
+           flash.now[:alert] = "登録に失敗しました"
+           render "new"
+        end
     end
     
 end
