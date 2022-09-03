88ddc4c066c7bda3a70d462314a4b7bec4dac654
ryoya-murata <ryoya.working@gmail.com>
2022-09-03 05:34:11 +0000

    Generate Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    # パスワードを安全に保存するための記述
+    has_secure_password
+    
+    # バリデーション
+    validates :name, presence: true, length: {maximum: 20}
+    validates :email, presence: true, uniqueness: true, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     root :to => 'books#index'
     
+    # 家計簿データ
     get "/books", to: "books#index"
     # 新規作成
     post "/books", to: "books#create"
@@ -11,6 +12,10 @@ Rails.application.routes.draw do
     patch "/books/:id", to: "books#update"
     # 削除
     delete "/books/:id", to: "books#destroy"
+    
+    # 会員登録
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
     
     # routeの自動生成
     # resources :books
