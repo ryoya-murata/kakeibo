9e2c94a68bce01dcf18cee12c2ca65d496fed2be
ryoya-murata <ryoya.working@gmail.com>
2022-09-04 13:27:28 +0000

    Change login text

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user.present? && user.authenticate(params[:password])
-            flash[:notice] = "ログインしました"
+            flash[:notice] = "ログインに成功しました"
             redirect_to books_path
         else
             flash.now[:alert] = "ログインに失敗しました"
